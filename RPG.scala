e750972aef10c1f9ebe9946bb0eb0393460e1703
Chiaki KUDO <chiakikudo@Chiaki-no-MacBook-Pro.local>
2015-02-06 22:27:55 +0900

    slime

--- RPG.scala
+++ RPG.scala
@@ -5,23 +5,36 @@ import java.lang.Class
 class RPG {
     def main(args: Array[String]){
         var Enemy = new Monster("スライム", Map(
-            HP -> 80,
-            MP -> 30,
-            attack -> 35,
-            defence -> 32, 
-            magic -> 24,
-            speed -> 21,
-            exp -> 40
+            "HP" -> 80,
+            "MP" -> 30,
+            "attack" -> 35,
+            "defence" -> 32, 
+            "magic" -> 24,
+            "speed" -> 21,
+            "exp" -> 40
         ), "ポーション")
         var Hero = new Ally("勇者", Map(
-            HP -> 130,
-            MP -> 60,
-            attack -> 54,
-            defence -> 48, 
-            magic -> 36,
-            speed -> 44
+            "HP" -> 130,
+            "MP" -> 60,
+            "attack" -> 54,
+            "defence" -> 48, 
+            "magic" -> 36,
+            "speed" -> 44
         ))
-        println(Enemy.name + "が現れた！")
+        
+        println("""
+                   ∩
+            　　　ノヽ
+            　　／　　＼
+            　 / (･)(･)∧
+            　｜(ヽ＿ノ)｜
+            　 ＼二二二／
+        　       """)
+        println("""
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+        ┃ """+ Enemy.name + """が現れた！       ┃
+        ┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+        """)
         Turn()
         
         def Turn() {
@@ -46,7 +59,7 @@ class RPG {
         }
     }
     
-    class Fighter(Name:String, Settings:Map[Int]){
+    class Fighter(Name:String, Settings:Map[String, Int]){
         val name = Name
         var HP = Settings.getOrElse("HP", 100)
         var MP = Settings.getOrElse("MP", 60)
@@ -59,41 +72,41 @@ class RPG {
         def attackedBy(Enemy:Fighter):(Int, Boolean) = {
             val Me = this
             val C = if(Enemy.isDefending) 0.7 else 1.0
-            val isCritical = Math.random() > 0.4
+            val isCritical = Math.random() > 0.9
             val criticalC = if(isCritical) 2.0 else 1.0
             val damage = (Me.attack - Enemy.defence/2) * criticalC * C
-            (damege.toInt, isCritical)
+            (damage.toInt, isCritical)
         }
         
         def act(n:Int, Enemy:Fighter){
             var Me = this
             n match{
-                case 1: 
+                case 1 => 
                     println(Me.name + "の攻撃！")
                     val damage = Enemy.attackedBy(Me)._1
                     val isCritical = Enemy.attackedBy(Me)._2
                     if(isCritical) println("会心の一撃！")
                     println(Enemy.name + "に" + damage + "のダメージ！")
                     Enemy.HP -= damage
-                case 2: Me.isDefending = true
-                case 3: Enemy.HP -= Enemy.magickedBy(Me)
-                case 4: if(Me.succeedRun) Finish
-                case _: 
+                case 2 => Me.isDefending = true
+                case 3 =>  Enemy.HP -= Enemy.attackedBy(Me)._1
+                case 4 => if(Me.succeedRun) true
+                case _ => true
             }
             if(Enemy.HP<=0){
                 println(Enemy.name + "は倒れた！")
-                if(Enemy.getClass.getName = "Monster"){
-                    println(Enemy.exp + "の経験値を手に入れた！")
-                    if(Math.random > 0.5){
+                if(Enemy.getClass.getName == "Monster"){
+                    //println(Enemy.exp + "の経験値を手に入れた！")
+                    /*if(Math.random > 0.5){
                         println("アイテム: " + Enemy.item + "を手に入れた！")
-                    }
+                    }*/
                 }
             }
-        }
+        }        
+        def succeedRun:Boolean = if(Math.random() > 0.6) true else false
     }
     
-    class Monster(Name:String, Settings:Map[Int], item:String) 
-    extends Fighter(Name:String, Settings:Map[Int]) {
+    class Monster(Name:String, Settings:Map[String, Int], item:String) extends Fighter(Name:String, Settings:Map[String, Int]) {
         val exp = Settings.getOrElse("exp", 10)    
     }
     
@@ -102,10 +115,8 @@ class RPG {
     
     }
     
-    class Ally extends Fighter {
-        def succeedRun:Boolean = {
+    class Ally(Name: String, Settings: Map[String,Int]) extends Fighter(Name: String, Settings: Map[String,Int]) {
         
-        }
     }
     
     
